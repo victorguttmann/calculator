d6a921f6be32695b60a3fc8cd63d819d7301e528
Student <example@example.com>
2019-08-19 10:45:28 -0700

    updated

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,24 @@
-var add = function(number1, number2) {
-  return number1 + number2;
+var num1 = parseInt(prompt("enter a number:"));
+var num2 = parseInt(prompt("enter another number"));
+
+var add = function() {
+  return(num1+num2);
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
+var subtract = function(){
+  return(num1-num2);
+}
 
-alert(add(number1, number2));
+var multiply = function(){
+  return(num1*num2);
+}
+
+var divide = function() {
+  return(num1/num2);
+}
+
+var remainder = function () {
+  return(num1%num2);
+}
+
+// alert(functionNAME(num1,num2));
